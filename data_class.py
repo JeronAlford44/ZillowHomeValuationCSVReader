dda592989d8b1869abc30e14a43cc5b87554bad8
JeronAlford44 <jeron.alford@gmail.com>
2023-12-10 14:55:47 -0800

    fixed API

--- data_class.py
+++ data_class.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials, storage
 from datetime import datetime
 import json
 
+
 cred = credentials.Certificate("capstoneadmin-ae398-firebase-adminsdk-3ns45-cddae79f61.json")
 firebase_admin.initialize_app(cred, {'storageBucket': 'capstoneadmin-ae398.appspot.com'})
 
@@ -15,27 +16,39 @@ class Data_Storage():
             return LookupError
 
         city_file_folder = f'data_by_city/cities{num_bedrooms}.json'
-        request = self.bucket.blob(city_file_folder).download_as_bytes()
-        return request
+        request = self.bucket.blob(city_file_folder).download_as_text()
+        json_data = json.loads(request)
+        for zipcode_dict in json_data:
+            if zipcode_dict.get('RegionName') == city_name:
+                return zipcode_dict
+        
+
+        return
 
     def fetch_zipcode_data(self, num_bedrooms: str, zip_code: str):
         if not num_bedrooms or not zip_code:
             return LookupError
 
         zipcode_file_folder = f'data_by_zipcode/zipcodes{num_bedrooms}.json'
-        request = self.bucket.blob(zipcode_file_folder).download_as_bytes()
-        return request
+        request = self.bucket.blob(zipcode_file_folder).download_as_text()
+        json_data = json.loads(request)
+        for zipcode_dict in json_data:
+            if zipcode_dict.get('RegionName') == zip_code:
+                return zipcode_dict
+        
 
-    def filter_one_date_per_year(self, data):
+        return
+
+    def filter_one_date_per_year(self, data: dict):
         try:
             # Assuming data is a JSON object with date-value pairs
-            data_arr = json.loads(data.decode('utf-8'))
+           
 
             # Extract the year from each date and keep track of seen years
             seen_years = set()
             filtered_data = {}
 
-            for date_str, value in data_arr[0].items():
+            for date_str, value in data.items():
                 try:
                     date_object = datetime.strptime(date_str, '%Y-%m-%d')
                     year = date_object.year
@@ -55,7 +68,10 @@ class Data_Storage():
 
 
 # Example usage
-# model = Data_Storage()
+model = Data_Storage()
 # city_data = model.fetch_city_data(num_bedrooms='3', city_name='Sacramento')
+# print(city_data)
+zipcode_data  = model.fetch_zipcode_data(num_bedrooms='3', zip_code='95691')
 # filtered_city_data = model.filter_one_date_per_year(city_data)
-# print(filtered_city_data)
+filtered_zipcode_data = model.filter_one_date_per_year(zipcode_data)
+print(filtered_zipcode_data)
